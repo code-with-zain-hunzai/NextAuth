02a311fac1df216b483cd5154ef98b258ecaa8a2
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-09-06 13:01:25 +0500

    fix credentials issue in user signIn

---
+++ src/lib/auth.ts
@@ -0,0 +1,48 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prismadb } from "./prismadb";
+import bcrypt from "bcrypt"; 
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prismadb),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/sign-in",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "jhon@example.com" }, 
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email and password are required");
+        }
+
+        const existingUser = await prismadb.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!existingUser) {
+          throw new Error("No user found with this email");
+        }
+
+        const passwordMatch = await bcrypt.compare(credentials.password, existingUser.password);
+        if (!passwordMatch) {
+          throw new Error("Incorrect password");
+        }
+
+        return {
+          id: `${existingUser.id}`,
+          username: existingUser.username,
+          email: existingUser.email,
+        };
+      },
+    }),
+  ],
+};
